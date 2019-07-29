f86f73ba7f818c7110de525c2f02467c6deb1126
sivadatta.bathina <sivadatta.bathina@cgi.com>
2019-07-29 10:38:31 +0530

    Task - 1 completed.

--- src/main/java/com/stackroute/App.java
+++ src/main/java/com/stackroute/App.java
@@ -1,5 +1,12 @@
 package com.stackroute;
 
+import com.stackroute.domain.Actor;
+import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+//        Get Movie through XMLBeanFactory
+        XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
+        Movie movieThroughXmlBeanFactory = xmlBeanFactory.getBean("movie", Movie.class);
+        System.out.println("Movie through XmlBeanFactory: " + movieThroughXmlBeanFactory);
+
+//        Get Movie through AppilicationContext
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Movie movie = context.getBean("movie", Movie.class);
+        System.out.println(movie.toString());
+
     }
 }
